4fe727867c9985956987d241d63c8f207742c216
Korepanov Pavel <korepanovpa01@gmail.com>
2021-11-23 15:47:44 +0400

    Исправлен класс вектор и класс матрица

--- CG Сourse Work/Matrix.cs
+++ CG Сourse Work/Matrix.cs
@@ -4,26 +4,16 @@ namespace CG_Сourse_Work
 {
     public class Matrix
     {
-        private readonly double[,] _matrix= new double[4, 4];
+        private readonly double[,] _matrix = new double[4, 4];
 
-        public Matrix() { }
-        
+        private Matrix()
+        {
+        }
+
         public double this[int i, int j]
         {
             get => _matrix[i, j];
-            set => _matrix[i, j] = value;
-        }
-
-        public static Matrix CreateUnitMatrix()
-        {
-            var matrix = new Matrix();
-            
-            for (var i = 0; i < 4; i++)
-            {
-                matrix[i, i] = 1.0;
-            }
-
-            return matrix;
+            private set => _matrix[i, j] = value;
         }
 
         public static Matrix operator *(Matrix firstMatrix, Matrix secondMatrix)
@@ -44,22 +34,22 @@ namespace CG_Сourse_Work
             return resultMatrix;
         }
 
-        public static Matrix CreateViewport(int x, int y, int w, int h, int depth = 255)
+        public static Matrix CreateYRotationMatrix(double rotationAngleInDegrees)
         {
-            return new Matrix
+            var rotationAngleInRadians = rotationAngleInDegrees * 0.0174533;
+
+            return new Matrix()
             {
-                [0, 0] = w / 2.0,
-                [1, 1] = h / -2.0,
-                [2, 2] = depth / 2.0,
-                [3, 3] = 1,
-                    
-                [0, 3] = x + w / 2.0,
-                [1, 3] = y + h / 2.0,
-                [2, 3] = depth / 2.0
+                [0, 0] = Math.Cos(rotationAngleInRadians),
+                [0, 2] = Math.Sin(rotationAngleInRadians),
+                [2, 0] = -Math.Sin(rotationAngleInRadians),
+                [2, 2] = Math.Cos(rotationAngleInRadians),
+                [1, 1] = 1,
+                [3, 3] = 1
             };
         }
 
-        public static Matrix CreateProjOrto(double l, double r, double b, double t, double n, double f)
+        public static Matrix CreateProjectionMatrix(double l, double r, double b, double t, double n, double f)
         {
             return new Matrix
             {
@@ -67,122 +57,99 @@ namespace CG_Сourse_Work
                 [1, 1] = 2.0 / (t - b),
                 [2, 2] = -2.0 / (f - n),
                 [3, 3] = 1,
-                
+
                 [0, 3] = -(r + l) / (r - l),
                 [1, 3] = -(t + b) / (t - b),
                 [2, 3] = -(f + n) / (f - n)
             };
         }
 
-        public static Matrix CreateLookAt(Vector eye, Vector center, Vector up)
+        public static Matrix CreateViewportMatrix(int x, int y, int w, int h, int depth = 255)
         {
-            var z = Vector.Normalize(eye - center);
-            var x = Vector.Normalize(Vector.ScalarMultiplication(up, z));
-            var y = Vector.Normalize(Vector.ScalarMultiplication(z, x));
-
             return new Matrix
             {
-                [0, 0] = x.X,
-                [0, 1] = x.Y,
-                [0, 2] = x.Z,
-
-
-                [1, 0] = y.X,
-                [1, 1] = y.Y,
-                [1, 2] = y.Z,
-
-                [2, 0] = z.X,
-                [2, 1] = z.Y,
-                [2, 2] = z.Z,
-
+                [0, 0] = w / 2.0,
+                [1, 1] = -h / 2.0,
+                [2, 2] = depth / 2.0,
                 [3, 3] = 1,
 
-                [0, 3] = -center.X,
-                [1, 3] = -center.Y,
-                [2, 3] = -center.Z
+                [0, 3] = x + w / 2.0,
+                [1, 3] = y + h / 2.0,
+                [2, 3] = depth / 2.0
             };
         }
 
-        public static Matrix CreateShiftMatrix(double tx, double ty, double tz)
+        public static Matrix CreateLookAtMatrix(Vector rightVector, Vector upVector, Vector forwardVector,
+            Vector cameraPosition)
         {
             return new Matrix
             {
-                [0, 0] = 1,
-                [1, 1] = 1,
-                [2, 2] = 1,
-                [3, 3] = 1,
-                    
-                [0, 3] = tx,
-                [1, 3] = ty,
-                [2, 3] = tz
-            };
-        }
+                [0, 0] = rightVector.X,
+                [0, 1] = rightVector.Y,
+                [0, 2] = rightVector.Z,
 
-        public static Matrix CreateScaleMatrix(double kx, double ky, double kz)
-        {
-            return new Matrix
-            {
-                [0, 0] = kx,
-                [1, 1] = ky,
-                [2, 2] = kz,
+                [1, 0] = upVector.X,
+                [1, 1] = upVector.Y,
+                [1, 2] = upVector.Z,
+
+                [2, 0] = forwardVector.X,
+                [2, 1] = forwardVector.Y,
+                [2, 2] = forwardVector.Z,
+
+                [0, 3] = -cameraPosition.X,
+                [1, 3] = -cameraPosition.Y,
+                [2, 3] = -cameraPosition.Z,
+
                 [3, 3] = 1
             };
         }
-        
-        public static Matrix CreateScaleMatrix(double k)
+
+        public static Matrix CreateUnitMatrix()
         {
-            return new Matrix
+            return new Matrix()
             {
                 [0, 0] = 1,
                 [1, 1] = 1,
                 [2, 2] = 1,
-                [3, 3] = k
+                [3, 3] = 1,
             };
         }
 
-        public static Matrix CreateRotateMatrix(double rotationAngleInDegrees, Rotation rotation)
-        {
-            var rotateMatrix = new Matrix()
-            {
-                [0, 0] = 1,
-                [1, 1] = 1,
-                [2, 2] = 1,
-                [3, 3] = 1
-            };
-            
-            var rotationAngleInRadians = rotationAngleInDegrees * 0.0174533;
-            switch (rotation)
-            {
-                case Rotation.X:
-                    rotateMatrix[0, 0] = Math.Cos(rotationAngleInRadians);
-                    rotateMatrix[1, 0] = Math.Sin(rotationAngleInRadians);
-                    rotateMatrix[0, 1] = -Math.Sin(rotationAngleInRadians);
-                    rotateMatrix[1, 1] = Math.Cos(rotationAngleInRadians);
-                    break;
-                
-                case Rotation.Y:
-                    rotateMatrix[1, 1] = Math.Cos(rotationAngleInRadians);
-                    rotateMatrix[2, 1] = Math.Sin(rotationAngleInRadians);
-                    rotateMatrix[1, 2] = -Math.Sin(rotationAngleInRadians);
-                    rotateMatrix[2, 2] = Math.Cos(rotationAngleInRadians);
-                    break;
-                
-                case Rotation.Z:
-                    rotateMatrix[0, 0] = Math.Cos(rotationAngleInRadians);
-                    rotateMatrix[2, 0] = Math.Sin(rotationAngleInRadians);
-                    rotateMatrix[0, 2] = -Math.Sin(rotationAngleInRadians);
-                    rotateMatrix[2, 2] = Math.Cos(rotationAngleInRadians);
-                    break;
-            }
-
-            return rotateMatrix;
-        }
-
-        public enum Rotation
-        {
-            X,
-            Y,
-            Z
-        }
+        // public static Matrix CreateShiftMatrix(double tx, double ty, double tz)
+        // {
+        //     return new Matrix
+        //     {
+        //         [0, 0] = 1,
+        //         [1, 1] = 1,
+        //         [2, 2] = 1,
+        //         [3, 3] = 1,
+        //
+        //         [0, 3] = tx,
+        //         [1, 3] = ty,
+        //         [2, 3] = tz
+        //     };
+        // }
+        //
+        // public static Matrix CreateScaleMatrix(double kx, double ky, double kz)
+        // {
+        //     return new Matrix
+        //     {
+        //         [0, 0] = kx,
+        //         [1, 1] = ky,
+        //         [2, 2] = kz,
+        //         [3, 3] = 1
+        //     };
+        // }
+        //
+        // public static Matrix CreateScaleMatrix(double k)
+        // {
+        //     return new Matrix
+        //     {
+        //         [0, 0] = 1,
+        //         [1, 1] = 1,
+        //         [2, 2] = 1,
+        //         [3, 3] = k
+        //     };
+        // }
     }
 }
--- CG Сourse Work/Vector.cs
+++ CG Сourse Work/Vector.cs
@@ -4,64 +4,56 @@ namespace CG_Сourse_Work
 {
     public class Vector
     {
-        private readonly double[] _array = new double[4];
-        
-        public double X { get => _array[0]; set => _array[0] = value; }
-        public double Y { get => _array[1]; set => _array[1] = value; }
-        public double Z { get => _array[2]; set => _array[2] = value; }
-        public double W { get => _array[3]; set => _array[3] = value; }
+        private readonly double[] _array = new double[3];
 
-        public Vector() { }
+        public double X
+        {
+            get => _array[0];
+            private set => _array[0] = value;
+        }
 
-        public Vector(double x, double y, double z, double w)
+        public double Y
+        {
+            get => _array[1];
+            private set => _array[1] = value;
+        }
+
+        public double Z
+        {
+            get => _array[2];
+            private set => _array[2] = value;
+        }
+
+        private double Length { get; }
+
+        public Vector()
+        {
+        }
+
+        public Vector(double x, double y, double z)
         {
             X = x;
             Y = y;
             Z = z;
-            W = w;
+            Length = Math.Sqrt(x * x + y * y + z * z);
         }
 
-        public static Vector operator *(Matrix matrix, Vector vector)
-        {
-            var resultVector = new Vector();
-            
-            for (var i = 0; i < 4; i++)
-            {
-                for (var j = 0; j < 4; j++)
-                {
-                    resultVector[i] += matrix[i, j] * vector[j];
-                }
-            }
-            
-            return resultVector;
-        }
-
-        public double this[int i]
+        private double this[int i]
         {
             get => _array[i];
             set => _array[i] = value;
         }
 
-        public static Vector ScalarMultiplication(Vector firstVector, Vector secondVector)
+        public Vector Normalized()
         {
-            return new Vector
+            if (Length == 0)
             {
-                X = firstVector.Y * secondVector.Z - firstVector.Z * secondVector.Y,
-                Y = firstVector.Z * secondVector.X - firstVector.X * secondVector.Z,
-                Z = firstVector.X * secondVector.Y - firstVector.Y * secondVector.X
-            };
+                return new Vector();
+            }
+
+            return new Vector(X / Length, Y / Length, Z / Length);
         }
 
-        public static Vector Clone(Vector vector)
-        {
-            return new Vector
-            {
-                X = vector.X,
-                Y = vector.Y,
-                Z = vector.Z
-            };
-        }
-        
         public static Vector operator -(Vector firstVector, Vector secondVector)
         {
             return new Vector
@@ -72,16 +64,39 @@ namespace CG_Сourse_Work
             };
         }
 
-        public double Length() => Math.Sqrt(X * X + Y * Y + Z * Z);
-
-        public static Vector Normalize(Vector vector)
+        public static Vector operator +(Vector firstVector, Vector secondVector)
         {
-            return new Vector()
+            return new Vector
             {
-                X = vector.X * (1.0 / vector.Length()),
-                Y = vector.Y * (1.0 / vector.Length()),
-                Z = vector.Z * (1.0 / vector.Length())
+                X = firstVector.X + secondVector.X,
+                Y = firstVector.Y + secondVector.Y,
+                Z = firstVector.Z + secondVector.Z
             };
+        }
+
+        public static Vector operator *(Matrix matrix, Vector vector)
+        {
+            var resultVector = new Vector();
+
+            for (var i = 0; i < 4; i++)
+            {
+                for (var j = 0; j < 4; j++)
+                {
+                    resultVector[i] += matrix[i, j] * vector[j];
+                }
+            }
+
+            return resultVector;
+        }
+
+        public static Vector ScalarMultiplication(Vector firstVector, Vector secondVector)
+        {
+            return new Vector
+            (
+                firstVector.Y * secondVector.Z - firstVector.Z * secondVector.Y,
+                firstVector.Z * secondVector.X - firstVector.X * secondVector.Z,
+                firstVector.X * secondVector.Y - firstVector.Y * secondVector.X
+            );
         }
     }
 }
